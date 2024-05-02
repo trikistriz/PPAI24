20e5f4280d3e77d8da14a1c602a8cb78fc9ea462
triki <trinimarcionni@gmail.com>
2024-05-02 12:35:07 -0300

    Creación de Entidades "Usuario" y "Vino". Armado parcial de pantalla inicial de Login.

---
+++ BE/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI24.BE
+{
+    public class Usuario
+    {
+		private string _constrasenia;
+		private string _nombre;
+		private string _premium;
+
+
+		public Usuario(string n_usuario, string password, string premium)
+		{
+			_nombre = n_usuario;
+			_constrasenia = password;
+			_premium = premium;
+		}
+		public string nombre
+		{
+			get { return _nombre; }
+			set { _nombre = value; }
+		}
+
+		public string contrasenia
+		{
+			get { return _constrasenia; }
+			set { _constrasenia = value; }
+		}
+		public string premium
+		{
+			get { return _premium; }
+			set { _premium = value; }
+		}
+
+
+	}
+}
---
+++ BE/Vino.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI24.BE
+{
+    public class Vino
+    {
+		private string _nombre;
+		private string _notaDeCataBodega;
+		private float _precioARS;
+		private string _aniada;
+
+		public Vino(string nombre, string notaDeCataBodega, float precioARS, string aniada)
+		{
+			_nombre = nombre;
+			_notaDeCataBodega = notaDeCataBodega;
+			_precioARS = precioARS;
+			_aniada = aniada;
+		}
+
+		public string aniada
+		{
+			get { return _aniada; }
+			set { _aniada = value; }
+		}
+
+		public float precioARS
+		{
+			get { return _precioARS; }
+			set { _precioARS = value; }
+		}
+
+		public string notaDeCataBodega
+		{
+			get { return _notaDeCataBodega; }
+			set { _notaDeCataBodega = value; }
+		}
+
+		public string nombre
+		{
+			get { return _nombre; }
+			set { _nombre = value; }
+		}
+
+
+	}
+}
